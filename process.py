350932b8bc490c3ed2a3ee17c59a75a80bea0b5a
JeremiasMartin <jmtnalv@gmail.com>
2022-03-21 14:18:20 -0300

    refactoring

--- process.py
+++ process.py
@@ -1,4 +1,3 @@
-from os import sep
 import subprocess
 import json
 import sys
@@ -6,6 +5,7 @@ import tempfile
 
 tmp_folder = tempfile.gettempdir()
 exiftool_exe = '.\exiftool.exe'
+# ffmpeg = '.\ffmpeg.exe' next impl
 tmp_output = f'{tmp_folder}\data.json'
 
 
@@ -18,40 +18,40 @@ def checkDictionary(i, string, dictionary):
 rtkflag = {}
 noRTK = {}
 rtk = {}
+total = {}
+cont = 0
 
 try:
+    sys.argv[1]
+    filename_output = input(
+        "Ingrese el nombre del archivo y presione ENTER: ")
 
-    for arg in sys.argv[1:]:
-        root_path = arg
-        folder = root_path.split(sep)[-1]
+    with open(f".\{filename_output}.json", "w") as outfile:
+        for arg in sys.argv[1:]:
+            root_path = arg
 
-        # Generating json file using exiftool (RtkFlag - cameraModel)
-        exiftool_command = [exiftool_exe, root_path,
-                            '-rtkflag', '-Model', '-q', '-json', '-fast', '-r', '-ext', 'JPG', '-ext', 'jpg','-ext','jpeg', '-ext', 'JPEG']  # https://www.exiftool.org/exiftool_pod.html#OPTIONS
+            # Generating json file using exiftool (RtkFlag - cameraModel)
+            exiftool_command = [exiftool_exe, root_path,
+                                '-rtkflag', '-Model', '-q', '-json', '-fast', '-r', '-ext', 'JPG', '-ext', 'jpg', '-ext', 'jpeg', '-ext', 'JPEG']  # https://www.exiftool.org/exiftool_pod.html#OPTIONS
 
-        with open(tmp_output, "w") as f:
-            process = subprocess.run(exiftool_command, stdout=f)
+            with open(tmp_output, "w") as f:
+                subprocess.run(exiftool_command, stdout=f)
 
-        with open(tmp_output) as f:
-            contenido = json.load(f)
+            with open(tmp_output) as f:
+                contenido = json.load(f)
 
-        for i in contenido:
-            if('RtkFlag' in i):
-                checkDictionary(i, "RtkFlag", rtkflag)
-                rtk[i["Model"]] = rtkflag
-            elif('Model' in i):
-                checkDictionary(i, "Model", noRTK)
+            for i in contenido:
+                if('RtkFlag' in i):
+                    checkDictionary(i, "RtkFlag", rtkflag)
+                    rtk[i["Model"]] = rtkflag
+                elif('Model' in i):
+                    checkDictionary(i, "Model", noRTK)
+            cont += len(contenido)
 
-        filename_output = input(
-            f"Ingrese el nombre del archivo json para la carpeta {folder}: ")
-
-        with open(f".\{filename_output}.json", "w") as outfile:
-            rtk.update(noRTK)
-            json.dump(rtk, outfile)
-
-        rtk.clear()
-        rtkflag.clear()
-        noRTK.clear()
+        total["TOTAL"] = cont
+        rtk.update(noRTK)
+        rtk.update(total)
+        json.dump(rtk, outfile)
 
 except IndexError:
     print("No file dropped")

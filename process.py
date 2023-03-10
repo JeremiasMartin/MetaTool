8c2dff098c8b76f849788da4416ae387dc10286a
JeremiasMartin <jmtnalv@gmail.com>
2023-03-10 13:32:09 -0300

    fix bug: generate additional file for videos

--- process.py
+++ process.py
@@ -25,45 +25,48 @@ noRTK = {}
 rtk = {}
 total = {}
 cont = 0
+img = False
 
 try:
 
     sys.argv[1]
     filename_output = input("Enter the output filename and press ENTER: ")
 
-    with open(os.path.join(".", f"{filename_output}.json"), "w") as outfile:
-        for arg in sys.argv[1:]:
-            if(not(arg.endswith(".MOV") | arg.endswith(".MP4"))):
-                exiftool_command = [EXIFTOOL_EXE, arg, '-rtkflag', '-Model', '-q', '-json', '-fast', '-r', '-ext', 'JPG', '-ext', 'jpg', '-ext', 'jpeg', '-ext', 'JPEG']
+    for arg in sys.argv[1:]:
+        if(not(arg.endswith(".MOV") | arg.endswith(".MP4"))):
+            img = True
+            exiftool_command = [EXIFTOOL_EXE, arg, '-rtkflag', '-Model', '-q', '-json', '-fast', '-r', '-ext', 'JPG', '-ext', 'jpg', '-ext', 'jpeg', '-ext', 'JPEG']
+            
+            # Run the exiftool command and store the output in a temporary file
+            with open(TEMP_OUTPUT_FILE, "w") as f:
+                subprocess.run(exiftool_command, stdout=f)
 
-                # Run the exiftool command and store the output in a temporary file
-                with open(TEMP_OUTPUT_FILE, "w") as f:
-                    subprocess.run(exiftool_command, stdout=f)
+            with open(TEMP_OUTPUT_FILE) as f:
+                contenido = json.load(f)
 
-                with open(TEMP_OUTPUT_FILE) as f:
-                    contenido = json.load(f)
+            for data in contenido:
+                if('RtkFlag' in data):
+                    checkDictionary(data, "RtkFlag", rtkflag)
+                    rtk[data["Model"]] = rtkflag
+                elif('Model' in data):
+                    checkDictionary(data, "Model", noRTK)
+            cont += len(contenido)
 
-                for data in contenido:
-                    if('RtkFlag' in data):
-                        checkDictionary(data, "RtkFlag", rtkflag)
-                        rtk[data["Model"]] = rtkflag
-                    elif('Model' in data):
-                        checkDictionary(data, "Model", noRTK)
-                cont += len(contenido)
-
-            elif((arg.endswith(".MP4")) | arg.endswith(".MOV")):
-                # If the file is an MP4 or MOV, convert it to SRT format
-                print("We detected an MP4/MOV file.")
-                stream = ffmpeg.input(arg)
-                stream = ffmpeg.output(stream, f'{filename_output}.SRT')
-                ffmpeg.run(stream)
+        elif((arg.endswith(".MP4")) | arg.endswith(".MOV")):
+            # If the file is an MP4 or MOV, convert it to SRT format
+            print("We detected an MP4/MOV file.")
+            stream = ffmpeg.input(arg)
+            stream = ffmpeg.output(stream, f'{filename_output}.SRT')
+            ffmpeg.run(stream)
         
         # Add the total count to the RTK dictionary
         total["TOTAL"] = cont
         rtk.update(noRTK)
         rtk.update(total)
 
-        json.dump(rtk, outfile)
+    if(img):
+        with open(os.path.join(".", f"{filename_output}.json"), "w") as outfile:
+            json.dump(rtk, outfile)
     
 except IndexError:
     print("No file dropped")

a2b02e837db3cb457597e0cae5e8349a0a936684
JeremiasMartin <jmtnalv@gmail.com>
2023-03-16 08:21:41 -0300

    FFmpeg process fixed

--- process.py
+++ process.py
@@ -1,73 +1,72 @@
 import json
-import tempfile
-import exiftool
 import os
 import sys
+import tempfile
+import exiftool
 import ffmpeg
 
-# Utiliza variables en lugar de valores constantes
-EXTENSIONS = ('.JPG', '.jpg', '.JPEG', '.jpeg')
+
+IMAGE_EXTENSIONS = ('.jpg', '.jpeg')
 PARAMS = ["Rtkflag", "Model"]
 TMP_FOLDER = tempfile.gettempdir()
 
 
-def check_value_in_dict(i, string, dictionary):
+
+def check_value_in_dict(tag_dict, tag_key, count_dict):
     '''
-    Check if the value is in the dictionary or not. If it's in the dict, increments its occurrence, otherwise adds it for the first time
+    Check if the tag value is in the dictionary or not. 
+    If it's in the dict, increments its occurrence, 
+    otherwise adds it for the first time
     '''
-    dictionary[i[string]] = 1 if i[string] not in dictionary else dictionary[i[string]] + 1
+    count_dict[tag_dict[tag_key]] = 1 if tag_dict[tag_key] not in count_dict else count_dict[tag_dict[tag_key]] + 1
+
 
 
 try:
     for arg in sys.argv[1:]:
-        root_path = arg
-        folder = os.path.basename(root_path)
 
-        rtkflag = {}
-        no_rtk = {}
-        rtk = {}
-        total = {}
-        cont = 0
         
-        for subdir, dirs, files in os.walk(root_path):
+        rtk_flag_count = {}
+        no_rtk_count = {}
+        rtk_model_count = {}
+        total_count = {}
+        num_files = 0
+        is_image = False
+        
+        ext = os.path.splitext(arg)[1]
 
-            for file in files:
+        
+        if os.path.isdir(arg):
+            for subdir, _, files in os.walk(arg):
+                for file in files:
+                    file_path = os.path.join(subdir, file)
+                    if os.path.splitext(file)[1].lower() in IMAGE_EXTENSIONS:
+                        is_image = True
+                        with exiftool.ExifToolHelper() as et: 
+                            metadata_dict = et.get_tags([file_path], PARAMS)
+                            for tag_dict in metadata_dict:
+                                if 'XMP:RtkFlag' in tag_dict:
+                                    check_value_in_dict(tag_dict, "XMP:RtkFlag", rtk_flag_count)
+                                    rtk_model_count[tag_dict["EXIF:Model"]] = rtk_flag_count
+                                elif("EXIF:Model" in tag_dict):
+                                    check_value_in_dict(tag_dict, "EXIF:Model", no_rtk_count)
 
-                filepath = os.path.join(subdir, file)
-                ext = os.path.splitext(file)[1]
+                            num_files += len(metadata_dict)
 
-                if ext in EXTENSIONS:
+        if os.path.splitext(arg)[1].lower() in ('.mp4', '.mov'):
+            print(f"We have detected a file with extension {ext}")
+            srt_file_name = input(f"Ingrese el nombre del archivo SRT: ")
+            stream = ffmpeg.input(arg)
+            stream = ffmpeg.output(stream, f'{srt_file_name}.SRT')
+            ffmpeg.run(stream)
 
-                    with exiftool.ExifToolHelper() as et:
-
-                        metadata = et.get_tags([filepath], PARAMS)
-
-                        for d in metadata:
-
-                            if 'XMP:RtkFlag' in d:
-                                check_value_in_dict(d, "XMP:RtkFlag", rtkflag)
-                                rtk[d["EXIF:Model"]] = rtkflag
-
-                            elif("EXIF:Model" in d):
-                                check_value_in_dict(d, "EXIF:Model", no_rtk)
-
-                        cont += len(metadata)
-
-                elif ext in ('.MP4', '.MOV'):
-                    print(f"Hemos detectado un archivo con extensión {ext}")
-                    filename_output = os.path.splitext(file)[0]
-                    output_path = os.path.join(TMP_FOLDER, f'{filename_output}.SRT')
-                    stream = ffmpeg.input(filepath)
-                    stream = ffmpeg.output(stream, output_path)
-                    ffmpeg.run(stream)
-
-        filename_output = input(f"Ingrese el nombre del archivo json para la carpeta {folder}: ")
-        with open(os.path.join('.', f"{filename_output}.json"), "w") as outfile:
-            total["TOTAL"] = cont
-            rtk.update(no_rtk)
-            rtk.update(total)
-            json.dump(rtk, outfile)
+        if is_image:
+            json_file_name = input(f"Enter the name of the JSON file: ")
+            with open(os.path.join('.', f"{json_file_name}.json"), "w") as outfile:
+                total_count["TOTAL"] = num_files
+                rtk_model_count.update(no_rtk_count)
+                rtk_model_count.update(total_count)
+                json.dump(rtk_model_count, outfile)
 
 except IndexError:
     print("No file dropped")
-
